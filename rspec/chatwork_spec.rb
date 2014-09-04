649ea48b1011aeed893775369b129280df6a01b6
Yasunori Tanaka <yasunori.tanaka@enish.com>
2014-09-04 19:09:07 +0900

    revise api

---
+++ rspec/chatwork_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+describe 'Authorize' do
+
+  it 'should return cookie when authorized' do
+
+    true.should == false
+  end
+end
--- src/API.rb
+++ src/API.rb
@@ -1,15 +1,19 @@
 require 'chatwork'
-require "./Weather"
+require_relative 'Weather'
 
 class API
   API_KEY = ''
   UID = ''
 
+  def initialize
+    @weather = Weather.new
+  end
+
   def notice_weather
-    weather = get_today_weather
+    weather = today_weather
 
     message = <<-EOS
-[To:#{UID}] Today's Weather
+[info][title] Today's Weather[/title]
 今日の天気
     Abstract: #{weather['weather']}
 
@@ -18,17 +22,21 @@ Rain Fall Chance
     06-12: #{weather['rain_fall_chance']['06-12']}
     12-18: #{weather['rain_fall_chance']['12-18']}
     18-24: #{weather['rain_fall_chance']['18-24']}
+[/info]
     EOS
 
     ChatWork.api_key = API_KEY
     ChatWork::Message.create(room_id: 12407368, body: "#{message}")
   end
 
+  def weekly_weather
+    @weather.weather?
+  end
+
   private
 
-  def get_today_weather
-    weather = Weather.new
-    weather.get_today_weather
+  def today_weather
+    @weather.get_today_weather
   end
 
   def fall_rain?
@@ -38,5 +46,6 @@ Rain Fall Chance
 end
 
 api = API.new
-api.notice_weather
-#p api.get_today_weather
+#api.notice_weather
+#p api.today_weather
+p api.weekly_weather
